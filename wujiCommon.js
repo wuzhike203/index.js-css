f3b87bed661c750a49c017912722416e36148fbc
wuji <95252845@qq.com>
2016-10-16 15:42:22 +0800

    add wujiCommon.js/less

--- wujiCommon.js
+++ wujiCommon.js
@@ -106,40 +106,6 @@ var EventUtil = {
     }
 };
 
-var moneyDetail = document.getElementById("tpay_moneyDetail");
-EventUtil.addHandler(moneyDetail, "click", billYesNo);
-
-function billYesNo(event){
-    /* 已付账单和未付账单切换显示 */
-    var e = EventUtil.getEvent(event);
-    var eTarget = EventUtil.getTarget(e);
-
-    if( eTarget.nodeName == "A"){
-        var subConYes = getElementsByClassName(moneyDetail, "tpay_subConYes")[0];
-        var subConNo = getElementsByClassName(moneyDetail, "tpay_subConNo")[0];
-        var tpay_item1 = getElementsByClassName(moneyDetail, "tpay_item1")[0];
-
-        if( classList.contains(eTarget.parentNode, "tpay_billYes") ){
-            classList.remove( subConYes, "tpay_hide");
-            classList.add(subConNo, "tpay_hide");
-            // 字体颜色切换
-            classList.remove(
-                    getElementsByClassName(tpay_item1, "tpay_colorT")[0], 
-                    "tpay_colorT");
-            classList.add( eTarget, "tpay_colorT");
-        }
-        if( classList.contains(eTarget.parentNode, "tpay_billNo")){
-            classList.add(subConYes, "tpay_hide");
-            classList.remove(subConNo, "tpay_hide");
-            // 字体颜色切换
-            classList.remove(
-                    getElementsByClassName(tpay_item1, "tpay_colorT")[0], 
-                    "tpay_colorT");
-            classList.add(eTarget, "tpay_colorT");
-        }
-    }
-}
-
 function getElementsByClassName(elem, cName){
     /* IE8- 不支持此方法 */
     if( "getElementsByClassName" in elem ){
